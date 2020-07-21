7d9d2e90d0118a663aa300e9c810c60cf248ff5d
Vincent <vincentyan8@hotmail.com>
2020-07-20 18:19:14 -0700

    Logic done

--- bigbraingame.py
+++ bigbraingame.py
@@ -18,17 +18,36 @@ def printGameBoard(array):
     for item in array:
         print(item)
 
-def move():
+
+def move(inp):
     global game_arr
-    game_arr[0].insert(0,game_arr[0][4])
-    game_arr[0].pop(5)
+    idx = int(inp[0])
+    print(idx)
+    if inp[1] is "R":
+        game_arr[idx].insert(0, game_arr[idx][4])
+        game_arr[idx].pop(5)
+    elif inp[1] is "L":
+        tmp_elem = game_arr[idx][0]
+        game_arr[idx].pop(0)
+        game_arr[idx].append(tmp_elem)
+    elif inp[1] is "U":
+        tmp = game_arr[0][idx]
+        for val in range(4):
+            game_arr[val][idx] = game_arr[val + 1][idx]
+        game_arr[4][idx] = tmp
+    else:
+        tmp_elem = game_arr[idx][4]
+        for var in range(4):
+            game_arr[var+1][idx] = game_arr[var][idx]
+        game_arr[0][idx] = tmp_elem
+
+
 
 printGameBoard(game_arr)
-
-
 
 print("after")
-move()
+move("1U")
+move("2U")
 printGameBoard(game_arr)
-
-
+window.geometry("1000x1000")
+window.mainloop()
